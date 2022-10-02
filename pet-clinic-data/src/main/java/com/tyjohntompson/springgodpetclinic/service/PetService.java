338320d58b485463ef94ef59471a8e9839852127
Dan Kozyrev <daniil.kozyrev@digitalchief.ru>
2022-10-02 23:23:49 +0300

    Added CrudService as simple CrudRepo realisation

---
+++ pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/service/PetService.java
@@ -0,0 +1,6 @@
+package com.tyjohntompson.springgodpetclinic.service;
+
+import com.tyjohntompson.springgodpetclinic.model.Pet;
+
+public interface PetService extends CrudService<Pet, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/service/VetService.java
@@ -0,0 +1,6 @@
+package com.tyjohntompson.springgodpetclinic.service;
+
+import com.tyjohntompson.springgodpetclinic.model.Vet;
+
+public interface VetService extends CrudService<Vet, Long>{
+}
