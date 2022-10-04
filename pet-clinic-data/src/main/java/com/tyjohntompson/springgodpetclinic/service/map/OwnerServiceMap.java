0ca82819e64e7529e7676a347cecb0ff1cbb39c1
Dan Kozyrev <daniil.kozyrev@digitalchief.ru>
2022-10-04 15:59:51 +0300

    Added DataLoader & refactored ServiceMaps for DI

--- pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/service/map/OwnerServiceMap.java
@@ -1,11 +1,13 @@
 package com.tyjohntompson.springgodpetclinic.service.map;
 
 import com.tyjohntompson.springgodpetclinic.model.Owner;
-import com.tyjohntompson.springgodpetclinic.service.CrudService;
+import com.tyjohntompson.springgodpetclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+@Service
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +31,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
