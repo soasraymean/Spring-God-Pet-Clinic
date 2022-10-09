74934d9cad2557c59590525aa50fdfeca8f3e5fb
Dan Kozyrev <daniil.kozyrev@digitalchief.ru>
2022-10-09 18:44:35 +0300

    Refactored AbstractMapService to Id generation

--- pet-clinic-webapp/src/main/java/com/tyjohntompson/springgodpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-webapp/src/main/java/com/tyjohntompson/springgodpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Dan");
         owner1.setLastName("Kozyrev");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Egor");
         owner2.setLastName("Creed");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Robert");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Michael");
         vet2.setLastName("Fork");
 
