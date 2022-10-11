35c25a8958cf4996c49651b3588252406efb11cf
Dan Kozyrev <daniil.kozyrev@digitalchief.ru>
2022-10-11 19:49:31 +0300

    Modified entities

--- pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/tyjohntompson/springgodpetclinic/model/Speciality.java
@@ -1,4 +1,14 @@
 package com.tyjohntompson.springgodpetclinic.model;
 
-public class Speciality extends BaseEntity{
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
