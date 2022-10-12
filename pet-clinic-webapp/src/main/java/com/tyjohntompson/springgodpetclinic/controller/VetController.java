d9788c054c1aac1c6765f079f4e0ab38e83e0195
Dan Kozyrev <daniil.kozyrev@digitalchief.ru>
2022-10-12 22:02:04 +0300

    Added notimplemented.html

--- pet-clinic-webapp/src/main/java/com/tyjohntompson/springgodpetclinic/controller/VetController.java
+++ pet-clinic-webapp/src/main/java/com/tyjohntompson/springgodpetclinic/controller/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "vets/vets", "vets/vets.html"})
+    @RequestMapping({"/vets", "vets/vets", "vets/vets.html", "/vets.html"})
     public String getVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
